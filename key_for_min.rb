68e4f59d45550f4864fc6d085b1a351bb985be8d
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-24 21:20:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@
 # return final lowest placeholder
 
 def key_for_min_value(name_hash)
-  name_hash = {:blake => 500, :ashley => 2, :adam => 1}
-   lowest_value = 0
+
+   lowest_value = nil
    lowest_key = nil
    name_hash.each do |key, value|
      if lowest_value == nil || value < lowest_value
